810e07b1f8c726713bf23d369925e82ab59e712d
krishnapriya-21 <you@example.com>
2025-09-13 18:16:01 +0530

    Deploy Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Replace 'Chef-Mistral-AI-React-WebApp' with your repository name
-  base: '/Chef-Mistral-AI-React-WebApp/',
   plugins: [react()],
 })
